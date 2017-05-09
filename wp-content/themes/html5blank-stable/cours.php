e664cb079266a18a3850b2f2140df6bfbabb0584
aboncenn <aurelien.boncenne@devinci.fr>
2017-05-09 14:55:49 +0200

    reparation des conflits

--- wp-content/themes/html5blank-stable/cours.php
+++ wp-content/themes/html5blank-stable/cours.php
@@ -7,8 +7,31 @@ Template Name: cours
     <div class="container">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <h2><?php the_title(); ?></h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php $image = wp_get_attachment_image_src(get_field('crossfit-img'), 'Miniature'); ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('crossfit-img')) ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php $image = wp_get_attachment_image_src(get_field('trx-img'), 'Miniature'); ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('trx-img')) ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php $image = wp_get_attachment_image_src(get_field('particulier-img'), 'Miniature'); ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('particulier-img')) ?>" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php $image = wp_get_attachment_image_src(get_field('groupe-img'), 'Miniature'); ?>
+                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('groupe-img')) ?>" />
+                </div>
+            </div>
 
-            <?php get_template_part('loop'); ?>
         <?php endwhile; endif; ?>
     </div>
 <?php get_footer(); ?>
