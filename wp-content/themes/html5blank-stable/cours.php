96e03b4def7dc8ca2779640ec33d3a56b3db69de
aboncenn <aurelien.boncenne@devinci.fr>
2017-05-11 12:27:05 +0200

    ajout page Cour et page Tarif

--- wp-content/themes/html5blank-stable/cours.php
+++ wp-content/themes/html5blank-stable/cours.php
@@ -6,29 +6,35 @@ Template Name: cours
 <?php get_header(); ?>
     <div class="container">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <h2><?php the_title(); ?></h2>
+            <div class="containerBox">
+                <h2 class="text-box"><?php the_title(); ?></h2>
+                <div class="containerImg"><?php $image = wp_get_attachment_image_src(get_field('background-titre-img'), 'Miniature'); ?></div>
+                <img class="img-responsive" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('background-titre-img')) ?>" />
+            </div>
             <div class="row">
-                <div class="col-md-12">
-                    <?php $image = wp_get_attachment_image_src(get_field('crossfit-img'), 'Miniature'); ?>
-                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('crossfit-img')) ?>" />
+                <div class="col-md-12 containerBox">
+                    <div class="textCour">
+                        <div class="background">
+                            <h4><?php the_field('titre_cours_1'); ?></h4>
+                                    <div class="gradien"></div>
+                                <p><?php the_field('text_1'); ?></p>
+                        </div>
+                    </div>
+                        <?php $image = wp_get_attachment_image_src(get_field('cour_1_img'), 'Miniature'); ?>
+                        <img class="img-responsive" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('cour_1_img')) ?>" />
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12">
-                    <?php $image = wp_get_attachment_image_src(get_field('trx-img'), 'Miniature'); ?>
-                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('trx-img')) ?>" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <?php $image = wp_get_attachment_image_src(get_field('particulier-img'), 'Miniature'); ?>
-                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('particulier-img')) ?>" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <?php $image = wp_get_attachment_image_src(get_field('groupe-img'), 'Miniature'); ?>
-                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('groupe-img')) ?>" />
+                <div class="col-md-12 containerBox">
+                    <div class="text2Cour">
+                        <div class="background">
+                            <h4><?php the_field('titre_cour_2'); ?>
+                                <div class="gradien2"></div>
+                                <p><?php the_field('texte_2'); ?></p>
+                        </div>
+                    </div>
+                    <?php $image = wp_get_attachment_image_src(get_field('img_cour_2'), 'Miniature'); ?>
+                    <img class="img-responsive" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('cour_1_img')) ?>" />
                 </div>
             </div>
 
--- wp-content/themes/html5blank-stable/tarif.php
+++ wp-content/themes/html5blank-stable/tarif.php
@@ -4,25 +4,44 @@ Template Name: tarif
  */
 ?>
 <?php get_header(); ?>
-<div class="container">
+<div class="container_tarif">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <div class="row">
-            <div class="col-md-6">
+        <div class="containerBox">
+            <h2 class="text-box"><?php the_title(); ?></h2>
+            <div class="containerImg"><?php $image = wp_get_attachment_image_src(get_field('image_background_title'), 'Miniature'); ?></div>
+            <img class="img-responsive" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_background_title')) ?>" />
+        </div>
+            <div class="Tarif_flex">
+                <h3>Les packs</h3>
+            </div>
+        <div class="offre_flex">
+            <a href="#" id="offre1" class=" col-md-3 containerTarif" >
+                <div class="textTarif">
+                    <h4 class="text_tarif_titre"><?php the_field('standard'); ?></h4>
+                    <p class="text_tarif_content"><?php the_field('prix_standard'); ?></p>
+
+                </div>
                 <?php $image = wp_get_attachment_image_src(get_field('image_1'), 'Miniature'); ?>
-                <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_1')) ?>" />
-            </div>
-            <div class="col-md-6">
+                <img class="img_tarif" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_1')) ?>" />
+            </a>
+            <a href="#" id="offre2" class="col-md-3 containerTarif">
+                <div class="textTarif">
+                    <h4  class="text_tarif_titre"><?php the_field('prenium'); ?></h4>
+                    <p class="text_tarif_content"><?php the_field('prix_prenium'); ?></p>
+                </div>
                 <?php $image = wp_get_attachment_image_src(get_field('image_2'), 'Miniature'); ?>
-                <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_2')) ?>" />
-            </div>
+                    <img class="img_tarif" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_2')) ?>" />
+            </a>
+            <a href="#" id="offre3" class="col-md-3 containerTarif">
+                <div class="textTarif">
+                    <h4  class="text_tarif_titre"><?php the_field('platinum'); ?></h4>
+                    <p class="text_tarif_content"><?php the_field('prix_platinium'); ?></p>
+                </div>
+                <?php $image = wp_get_attachment_image_src(get_field('image_3'), 'Miniature'); ?>
+                <img class="img_tarif" src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_3')) ?>" />
+            </a>
         </div>
-        <div class="row">
-            <?php $image = wp_get_attachment_image_src(get_field('image_3'), 'Miniature'); ?>
-            <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(get_field('image_3')) ?>" />
-        </div>
-        <div class="row">
-            <?php the_content(); ?>
+        <div class="row tarif-content" id="text-content">
         </div>
 
     <?php endwhile; endif; ?>
